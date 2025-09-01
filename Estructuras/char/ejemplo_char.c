9615e220fbb33b2af8281ee23c157bc1f0ace2a7
EdwardPin <eepinzonp@academia.usbbog.edu.co>
2025-09-01 20:43:44 +0000

    Sex

--- Estructuras/char/ejemplo_char.c
+++ Estructuras/char/ejemplo_char.c
@@ -4,4 +4,5 @@ int main() { // Función principal
     char letra = 'A'; // Declara una variable char 'letra' y le asigna el carácter 'A'
     printf("El carácter es: %c y su código ASCII es: %d\n", letra, letra); // Imprime el carácter y su código ASCII
     return 0; // Indica que el programa terminó correctamente
+    // El %c es para mostrar el char, y el %d\n muestra la posicion en la tabla ASCII
 }
